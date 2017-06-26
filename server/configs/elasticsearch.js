78ec6c273ec85fdf3c82f952eec71ceb7116cdee
XuHaoJun <XuhaojunTW@gmail.com>
2017-06-26 18:03:48 +0800

    Update elasticsearch config default options.

--- server/configs/elasticsearch.js
+++ server/configs/elasticsearch.js
@@ -6,15 +6,20 @@ const developmentUrl = Url.parse(developmentUrlString);
 const productionUrlString = (process.env.BONSAI_URL || developmentUrlString);
 const productionUrl = Url.parse(productionUrlString);
 
+// FIX
+// can't directly pass url because mongoosastic overide elasticsearch constructor params.
 function urlToConfig(url) {
-  const {hostname, port, auth, protocol} = url;
+  const {hostname, port, auth} = url;
+  const protocol = url
+    .protocol
+    .replace(':', '');
   return {
     host: hostname,
-    port: parseInt(port, 10) || undefined,
+    port: parseInt(port, 10) || (protocol === 'https'
+      ? 443
+      : 80),
     auth: auth || undefined,
-    protocol: protocol
-      ? protocol.replace(':', '')
-      : 'http'
+    protocol: protocol || 'http'
   };
 }
 
