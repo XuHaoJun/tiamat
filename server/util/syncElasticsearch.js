6d66ca347d7b682418fe092f58f878e8aa6de050
XuHaoJun <XuhaojunTW@gmail.com>
2017-06-26 19:00:54 +0800

    Log syncElasticsearch

--- server/util/syncElasticsearch.js
+++ server/util/syncElasticsearch.js
@@ -2,6 +2,18 @@ import Discussion from '../models/discussion';
 import ForumBoard from '../models/forumBoard';
 
 export default function syncElasticsearch() {
-  ForumBoard.synchronize();
-  Discussion.synchronize();
+  const s1 = ForumBoard.synchronize();
+  const s2 = Discussion.synchronize();
+  [s1, s2].forEach((stream) => {
+    let count = 0;
+    stream.on('data', function(err, doc) {
+      count += 1;
+    });
+    stream.on('close', function() {
+      console.log('indexed ' + count + ' documents!');
+    });
+    stream.on('error', function(err) {
+      console.log(err);
+    });
+  });
 }
