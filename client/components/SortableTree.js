e8861bf72ecd2c242fa04ba20fa06dbe26dea94e
XuHaoJun <XuhaojunTW@gmail.com>
2018-02-12 23:54:34 +0800

    Bump react-sortable-tree.

--- client/components/SortableTree.js
+++ client/components/SortableTree.js
@@ -2,52 +2,50 @@ import React from "react";
 import Loadable from "react-loadable";
 
 function Loading() {
-  return <div>loading...</div>;
+  return <div>Loading...</div>;
 }
 
-// FIXME
-// SortableTree can't work with server-side so that show loading only.
 const SortableTree = Loadable({
-  loader: () => {
-    if (typeof window === "undefined") {
-      return Promise.resolve(Loading);
+  loader: async () => {
+    if (process.browser) {
+      const modules = await Promise.all([
+        import(/* webpackChunkName: "react-dnd-touch-backend" */ "react-dnd-touch-backend"),
+        import(/* webpackChunkName: "react-dnd-html5-backend" */ "react-dnd-html5-backend"),
+        import(/* webpackChunkName: "react-dnd-multi-backend" */ "react-dnd-multi-backend"),
+        import(/* webpackChunkName: "react-dnd" */ "react-dnd"),
+        import(/* webpackChunkName: "react-sortable-tree" */ "react-sortable-tree"),
+        import(/* webpackChunkName: "react-sortable-tree/style.css" */ "../../node_modules/react-sortable-tree/style.css")
+      ]);
+      const [
+        TouchBackend,
+        HTML5Backend,
+        MultiBackend,
+        reactDnd,
+        reactSortableTree
+      ] = modules;
+      const { TouchTransition } = MultiBackend;
+      const { DragDropContext } = reactDnd;
+      const HTML5toTouch = {
+        backends: [
+          {
+            backend: HTML5Backend.default
+          },
+          {
+            backend: TouchBackend.default({
+              delayTouchStart: 10
+            }), // Note that you can call your backends with options
+            preview: true,
+            transition: TouchTransition
+          }
+        ]
+      };
+      const { SortableTreeWithoutDndContext } = reactSortableTree;
+      const SortableTreeComponent = DragDropContext(
+        MultiBackend.default(HTML5toTouch)
+      )(SortableTreeWithoutDndContext);
+      return SortableTreeComponent;
     } else {
-      return Promise.all([
-        import("react-dnd-touch-backend"),
-        import("react-dnd-html5-backend"),
-        import("react-dnd-multi-backend"),
-        import("react-dnd"),
-        import("react-sortable-tree")
-      ]).then(result => {
-        const [
-          TouchBackend,
-          HTML5Backend,
-          MultiBackend,
-          reactDnd,
-          reactSortableTree
-        ] = result;
-        const { TouchTransition } = MultiBackend;
-        const { DragDropContext } = reactDnd;
-        const HTML5toTouch = {
-          backends: [
-            {
-              backend: HTML5Backend.default
-            },
-            {
-              backend: TouchBackend.default({
-                delayTouchStart: 10
-              }), // Note that you can call your backends with options
-              preview: true,
-              transition: TouchTransition
-            }
-          ]
-        };
-        const { SortableTreeWithoutDndContext } = reactSortableTree;
-        const sortableTreeComponent = DragDropContext(
-          MultiBackend.default(HTML5toTouch)
-        )(SortableTreeWithoutDndContext);
-        return sortableTreeComponent;
-      });
+      return Loading;
     }
   },
   loading: Loading
